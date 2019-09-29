5e50ab77ef74bd2eb7596e56e10ce92016874dda
Shaheer <shaheerramzan1619@gmail.com>
2019-09-29 22:45:09 +0500

    article table, model and validation added

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 5, maximum: 20 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 50 }
+end
