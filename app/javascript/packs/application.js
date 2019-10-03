88c89b6c43360438ab3cab53092af513baaebe26
Shaheer <shaheerramzan1619@gmail.com>
2019-10-03 16:46:51 +0500

    Home page and footer added nad view added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-//require("jquery")
+require("jquery")
 require("bootstrap")
 require("turbolinks").start()
 require("@rails/activestorage").start()
